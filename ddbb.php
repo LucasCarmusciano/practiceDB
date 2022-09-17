29dbb57105855b018546b18b304eacaf592f35f5
LucasCarmusciano <lucascarmusciano@gmail.com>
2022-09-17 19:25:53 -0300

    genero una funcion para insertar a la lista

--- ddbb.php
+++ ddbb.php
@@ -1,11 +1,20 @@
 <?php 
+require_once "home.php";
+
 function connect(){
     return new PDO('mysql:host=localhost;'.'dbname=listaNombres;charset=utf8', 'root', '');
 }
+
 function getList(){
     $db = connect();
     $query = $db->prepare('SELECT * FROM nombres');
     $query->execute();
     return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+function insertList($nombre, $apellido, $edad){
+    $db = connect();
+    $query = $db->prepare("INSERT INTO nombres(name,lastname, age) VALUES (?,?,?)");
+    $query->execute([$nombre, $apellido, $edad]);
 }
 ?>
